8c2ab65b775422854bb50824bf948ca89bff4a51
BertoPolo <bertopolo91@gmail.com>
2022-02-09 16:49:46 +0100

    asd

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,23 +1,50 @@
 import books from "../data/horror.json";
 import SingleBook from "../components/SingleBook";
 import { Container, Row } from "react-bootstrap";
+import { Component } from "react";
 
-const BookList = (props) => {
-  return (
-    <Container>
-      <Row>
-        {books.map((books) => (
-          <SingleBook
-            key={books.asin}
-            title={books.title}
-            img={books.img}
-            price={books.price}
-            category={books.category}
-          ></SingleBook>
-        ))}
-      </Row>
-    </Container>
-  );
-};
+class BookList extends Component {
+  state = {
+    searchInput: "",
+  };
+  handleChange = (value) => {
+    this.setState({
+      searchInput: value,
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <Container>
+          <input
+            type="text"
+            onChange={(e) => this.handleChange(e.target.value)}
+          />
+        </Container>
+
+        <Container>
+          <Row>
+            {books
+              .filter((book) =>
+                book.title
+                  .toLowerCase()
+                  .includes(this.state.searchInput.toLowerCase())
+              )
+              .map((book) => (
+                <SingleBook
+                  key={book.asin}
+                  title={book.title}
+                  img={book.img}
+                  price={book.price}
+                  category={book.category}
+                ></SingleBook>
+              ))}
+          </Row>
+        </Container>
+      </>
+    );
+  }
+}
 
 export default BookList;
